a76a55c498938a75a8b76c8866f522e5c921c407
Andi Sultan <andisultan.mail@gmail.com>
2022-02-21 22:42:18 +0800

    u

--- js-mini-apps/2022-02-17-etch-a-sketch/index.js
+++ js-mini-apps/2022-02-17-etch-a-sketch/index.js
@@ -1,1 +1,9 @@
 // from theodinproject
+// https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/etch-a-sketch-project
+let grid = document.querySelector("#grid");
+let cell = document.createElement("div");
+cell.className = "cell";
+
+for (let i = 0; i < 16; i++) {
+  grid.appendChild(cell.cloneNode(true));
+}
