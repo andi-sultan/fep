674946479aecf8c607b31b7abb10e4ec8245e4c8
Andi Sultan <andisultan.mail@gmail.com>
2022-02-28 22:07:29 +0800

    2022-02-17-etch-a-sketch

--- js-mini-apps/2022-02-17-etch-a-sketch/index.js
+++ js-mini-apps/2022-02-17-etch-a-sketch/index.js
@@ -1,24 +1,42 @@
 // from theodinproject
 // https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/etch-a-sketch-project
-let grid = document.querySelector("#grid");
-let cell = document.createElement("div");
-cell.className = "cell";
 
-for (let i = 0; i < 16; i++) {
-  grid.appendChild(cell.cloneNode(true));
+let gridSize = 50;
+let cellFillColor = "#000";
+let grid = document.querySelector("#grid");
+const btnClear = document.querySelector("#clear");
+const btnRgb = document.querySelector("#rgb");
+const btnDarker = document.querySelector("#darker");
+
+function createCells(n) {
+  grid.innerHTML = "";
+  n = n > 100 ? 100 : n;
+  totalCells = n ** 2;
+  gridCellWidth = `${100 / n}%`;
+  for (let i = 0; i < totalCells; i++) {
+    const cell = document.createElement("div");
+    cell.className = "cell";
+    cell.style.flex = ` 0 0 ${gridCellWidth}`;
+    cell.addEventListener("mouseover", (e) => {
+      e.target.style.backgroundColor = cellFillColor;
+    });
+    grid.appendChild(cell);
+  }
 }
 
-// todo:
-// vars&elems:
-// btn clear
-// btn rgb
-// btn darker
+function setCellFillColor(c) {
+  cellFillColor = c;
+}
 
-// elem grid
-// grid size
-// elem cell
-// cell fill color
+// clear event
+btnClear.addEventListener("click", () => {
+  gridnum = prompt("gridSize?");
+  gridSize = 20;
+  createCells(gridSize);
+});
+// // rgb event
+// btnRgb.addEventListener();
+// // darker event
+// btnDarker.addEventListener();
 
-
-// functions:
-// set cell fill color
+createCells(gridSize);
