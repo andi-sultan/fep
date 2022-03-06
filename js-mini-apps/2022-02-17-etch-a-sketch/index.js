d01b32fe899eb8623317b9b6f501c20a7becbb3d
Andi Sultan <andisultan.mail@gmail.com>
2022-03-06 15:40:41 +0800

    2022-02-17-etch-a-sketch|finish solution

--- js-mini-apps/2022-02-17-etch-a-sketch/index.js
+++ js-mini-apps/2022-02-17-etch-a-sketch/index.js
@@ -3,6 +3,8 @@
 
 let gridSize = 50;
 let cellFillColor = "#000";
+let fillColorMode = "default";
+let darkerCount = 0;
 let grid = document.querySelector("#grid");
 const btnClear = document.querySelector("#clear");
 const btnRgb = document.querySelector("#rgb");
@@ -18,9 +20,23 @@ function createCells(n) {
     cell.className = "cell";
     cell.style.flex = ` 0 0 ${gridCellWidth}`;
     cell.addEventListener("mouseover", (e) => {
-      e.target.style.backgroundColor = cellFillColor;
+      fillCellColor(e.target.style);
     });
     grid.appendChild(cell);
+  }
+}
+
+function fillCellColor(element) {
+  if (fillColorMode == "rgb") {
+    let colorr = Math.floor(Math.random() * 16777215).toString(16);
+    let colorrr = `#${colorr}`;
+    element.backgroundColor = colorrr;
+  } else if (fillColorMode == "darker") {
+    let darkerColor = `hsl(0,0%,${100 - darkerCount * 10}%)`;
+    darkerCount = darkerCount + 1 > 10 ? 10 : darkerCount + 1;
+    element.backgroundColor = darkerColor;
+  } else {
+    element.backgroundColor = cellFillColor;
   }
 }
 
@@ -30,6 +46,8 @@ function setCellFillColor(c) {
 
 // clear event
 btnClear.addEventListener("click", () => {
+  fillColorMode = "default";
+  darkerCount = 0;
   gridNum = null;
   gridNum = prompt("gridSize?");
   while (isNaN(gridNum) || gridNum > 100 || gridNum < 1) {
@@ -44,9 +62,14 @@ btnClear.addEventListener("click", () => {
   gridSize = !isNaN(gridNum) ? gridNum : 4;
   createCells(gridSize);
 });
-// todo: rgb event
-// btnRgb.addEventListener();
+
+btnRgb.addEventListener("click", () => {
+  fillColorMode = "rgb";
+});
 // todo: darker event
-// btnDarker.addEventListener();
+btnDarker.addEventListener("click", () => {
+  fillColorMode = "darker";
+  darkerCount = 0;
+});
 
 createCells(gridSize);
