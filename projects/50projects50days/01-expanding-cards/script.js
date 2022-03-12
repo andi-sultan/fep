f7a3a87e890f1bfe52ade00f71a643a8ce33b79c
Andi Sultan <andisultan.mail@gmail.com>
2022-03-13 01:40:26 +0800

    01-expanding-cards refactor

--- projects/50projects50days/01-expanding-cards/script.js
+++ projects/50projects50days/01-expanding-cards/script.js
@@ -1,9 +1,9 @@
-const panels=document.querySelectorAll('.panel')
-panels.forEach(panel=>{
-    panel.addEventListener('click',(e)=>{
-        panels.forEach(panel=>{
-            panel.classList.remove('active')
-        })
-        e.target.classList.add('active')
-    })
-})
+const panels = document.querySelectorAll(".expc-panel");
+panels.forEach((panel) => {
+  panel.addEventListener("click", (e) => {
+    panels.forEach((panel) => {
+      panel.classList.remove("active");
+    });
+    e.target.classList.add("active");
+  });
+});
