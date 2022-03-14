32f137f822e0e6b2b426db420421687827cdcad6
Andi Sultan <andisultan.mail@gmail.com>
2022-03-14 22:25:09 +0800

    02-progress-steps|refactor

--- projects/50projects50days/02-progress-steps/script.js
+++ projects/50projects50days/02-progress-steps/script.js
@@ -1,7 +1,7 @@
-const progress = document.getElementById("progress");
-const prev = document.getElementById("prev");
-const next = document.getElementById("next");
-const circles = document.querySelectorAll(".circle");
+const progress = document.getElementById("progress-line");
+const prev = document.getElementById("progress-prev");
+const next = document.getElementById("progress-next");
+const circles = document.querySelectorAll(".progress-circle");
 
 let currentActive = 1;
 
