b6362a4c1c5fd732f7f59e587d0283c0b56c37cf
Andi Sultan <andisultan.mail@gmail.com>
2022-03-20 14:35:05 +0800

    2022-03-18-calculator|create interface

---
+++ js-mini-apps/2022-03-18-calculator/script.js
@@ -0,0 +1,16 @@
+// data butns
+
+// data temp num element
+// data hasil num element
+
+// calc functions
+
+// var to store temp num
+// var to store operation (+_*/)
+// var to store hasil
+// var to store last clicked button
+
+
+
+// num btn click:
+    // if
