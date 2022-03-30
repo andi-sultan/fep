e40a7dc3a25d0819367152df180d520305e85ce2
Andi Sultan <andisultan.mail@gmail.com>
2022-03-30 22:17:43 +0800

    2022-03-26-tic-tac-toe|recreate using module patter (not finis)

--- js-mini-apps/2022-03-26-tic-tac-toe/script.js
+++ js-mini-apps/2022-03-26-tic-tac-toe/script.js
@@ -1,52 +1,55 @@
-const modal = document.getElementById("ttt-gameModal");
-const display = document.getElementById("ttt-display");
-const start = document.getElementById("ttt-start");
-
-const player1Input = document.getElementById("ttt-input-player1");
-const player2Input = document.getElementById("ttt-input-player2");
-
-start.addEventListener("click", function () {
-  modal.classList.add("hide");
-  display.classList.add("show");
-});
-
-const Player = (name) => {
-  const getName = () => name;
-  return { getName };
-};
+const gameTiles = Array.from(document.querySelectorAll(".ttt-box"));
 
 const Game = (() => {
-  const gameBoard = ["", "", "", "", "", "", "", "", ""];
-  const winningConditions = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-
-  const getPlayer = () => {};
-
-  const checkWinner = () => {
-    
+  let player = "X";
+  let isGameActive = false;
+  const startGame = (tiles) => {
+    Board.setTiles(tiles);
+    isGameActive = true;
   };
 
-  const getPlayerTurn = () => {};
+  const isActive = () => isGameActive;
 
-  const restart = () => {};
+  const changePlayer = () => {
+    if (player === "X") player = "O";
+    else if (player === "O") player = "X";
+  };
 
-  const start = () => {};
+  const getPlayerName = () => player;
 
-  return { getPlayer, checkWinner, getPlayerTurn, restart, start };
+  // todo: cek winner, set player name
+  return { startGame, isActive, changePlayer, getPlayerName };
 })();
 
-const Display = (() => {
-  const getPlayerInput = () => {};
+const Board = (() => {
+  let board = ["", "", "", "", "", "", "", "", ""];
+  const updateBoard = (board, tile, index) => {
+    const playerName = Game.getPlayerName();
+    board[index] = playerName;
 
-  const render = () => {};
+    if (isEmptyTile(tile) && Game.isActive()) render(tile, playerName);
+    // todo: Game.handleResultValidation()
+    Game.changePlayer();
+  };
 
-  return { getPlayerInput, render };
+  const isEmptyTile = (tile) => {
+    if (tile.innerText === "X" || tile.innerText === "O") {
+      return false;
+    }
+    return true;
+  };
+
+  const render = (tile, playerName) => {
+    tile.innerText = playerName;
+  };
+
+  const setTiles = (tiles) => {
+    tiles.forEach((tile, index) => {
+      tile.addEventListener("click", () => updateBoard(board, tile, index));
+    });
+  };
+
+  return { setTiles };
 })();
+
+Game.startGame(gameTiles);
