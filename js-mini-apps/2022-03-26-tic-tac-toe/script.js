cc85b5d5a1922b8bfad6befda71e2f24cf539d0c
Andi Sultan <andisultan.mail@gmail.com>
2022-04-02 22:30:32 +0800

    2022-03-26-tic-tac-toe|finis

--- js-mini-apps/2022-03-26-tic-tac-toe/script.js
+++ js-mini-apps/2022-03-26-tic-tac-toe/script.js
@@ -1,5 +1,21 @@
+const modal = document.querySelector("#ttt-gameModal");
+const display = document.getElementById("ttt-display");
+const start = document.querySelector("#ttt-start");
+const player1Label = document.getElementById("ttt-player1");
+const player2Label = document.getElementById("ttt-player2");
+
+start.addEventListener("click", () => {
+  const player1Input = document.getElementById("ttt-input-player1").value;
+  const player2Input = document.getElementById("ttt-input-player2").value;
+  Game.startGame(player1Input, player2Input);
+  modal.classList.add("hide");
+  display.classList.add("show");
+});
+
 const Game = (() => {
-  let player = "X";
+  let player;
+  let player1 = { label: "X" };
+  let player2 = { label: "O" };
   let isGameActive = false;
   /*
     Indexes within the board
@@ -18,18 +34,24 @@ const Game = (() => {
     [2, 4, 6],
   ];
 
-  const startGame = () => {
+  const startGame = (p1name, p2name) => {
     isGameActive = true;
+    player1.name = p1name ? p1name : "P1";
+    player2.name = p2name ? p2name : "P2";
+    player = player1;
+    player1Label.textContent = `Player 1: ${player1.name} (${player1.label})`;
+    player2Label.textContent = `Player 2: ${player2.name} (${player2.label})`;
+    Board.renderPlayerTurn(player);
   };
 
   const isActive = () => isGameActive;
 
   const changePlayer = () => {
-    if (player === "X") player = "O";
-    else if (player === "O") player = "X";
+    if (player.label === "X") player = player2;
+    else if (player.label === "O") player = player1;
   };
 
-  const getPlayerName = () => player;
+  const getPlayer = () => player;
 
   const handleResultValidation = (board) => {
     let roundWon = false;
@@ -53,19 +75,32 @@ const Game = (() => {
       winner = player;
     }
 
-    if (!board.includes("")) {
+    const boardIsFull = !board.includes("");
+    if (boardIsFull && isGameActive) {
       winner = "TIE";
     }
+
     changePlayer();
-    if (winner) Board.announceWinner(winner);
+
+    if (winner) {
+      Board.announceWinner(winner);
+    } else {
+      Board.renderPlayerTurn(player);
+    }
+  };
+
+  const reset = () => {
+    Board.reset();
+    startGame(player1.name, player2.name);
   };
 
   return {
     startGame,
     isActive,
     changePlayer,
-    getPlayerName,
+    getPlayer,
     handleResultValidation,
+    reset,
   };
 })();
 
@@ -73,13 +108,14 @@ const Board = (() => {
   let board = ["", "", "", "", "", "", "", "", ""];
   const tiles = Array.from(document.querySelectorAll(".ttt-box"));
   const winnerLabel = document.querySelector("#ttt-winner");
+  const playerTurnLabel = document.querySelector("#ttt-plyr-turn");
 
   const updateBoard = (board, tile, index) => {
-    const playerName = Game.getPlayerName();
+    const player = Game.getPlayer();
 
     if (isEmptyTile(tile) && Game.isActive()) {
-      render(tile, playerName);
-      board[index] = playerName;
+      render(tile, player.label);
+      board[index] = player.label;
       Game.handleResultValidation(board);
     }
   };
@@ -91,8 +127,12 @@ const Board = (() => {
     return true;
   };
 
-  const render = (tile, playerName) => {
-    tile.innerText = playerName;
+  const render = (tile, playerLabel) => {
+    tile.textContent = playerLabel;
+  };
+
+  const renderPlayerTurn = (player) => {
+    playerTurnLabel.textContent = `${player.name}'s turn (${player.label})...`;
   };
 
   tiles.forEach((tile, index) => {
@@ -100,10 +140,30 @@ const Board = (() => {
   });
 
   const announceWinner = (winner) => {
-    winnerLabel.textContent = `The winner is ${winner}`;
+    if (winner === "TIE") {
+      winnerLabel.textContent = `It's a ${winner}`;
+    } else {
+      winnerLabel.textContent = `The winner is ${winner.name} (${winner.label})`;
+    }
+    playerTurnLabel.textContent = "";
+    winnerLabel.classList.remove("hide");
   };
 
-  return { announceWinner };
+  const reset = () => {
+    board = ["", "", "", "", "", "", "", "", ""];
+    tiles.forEach((tile) => {
+      tile.textContent = "";
+    });
+    winnerLabel.textContent = "";
+    playerTurnLabel.textContent = "";
+  };
+
+  return { renderPlayerTurn, announceWinner, reset };
 })();
 
-Game.startGame();
+const Controls = (() => {
+  const reset = document.querySelector("#ttt-reset");
+  reset.addEventListener("click", () => {
+    Game.reset();
+  });
+})();
