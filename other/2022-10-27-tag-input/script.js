ce5f3576464790dddb6c37b79f4f501f47f19e3b
Andi Sultan <andisultan317@gmail.com>
2022-10-28 23:51:33 +0800

    2022-10-27-tag-input | prevent duplicate input

--- other/2022-10-27-tag-input/script.js
+++ other/2022-10-27-tag-input/script.js
@@ -1,56 +1,60 @@
-const tagComponents=document.querySelectorAll('.tag')
-// todo: put in one foreach
+const tagComponents = document.querySelectorAll(".tag");
 
-const tagContainer = document.querySelector(".tag-container");
-const input = document.querySelector(".tag-container input");
+tagComponents.forEach((tag) => {
+  const tagContainer = tag.querySelector(".tag-inner");
+  const input = tag.querySelector(".tag-input");
 
-let tags = [];
+  let tagsData = [];
 
-function createTag(label) {
-  const div = document.createElement("div");
-  div.setAttribute("class", "tag");
-  const span = document.createElement("span");
-  span.innerHTML = label;
-  const closeBtn = document.createElement("i");
-  closeBtn.setAttribute("class", "fas fa-times");
-  closeBtn.setAttribute("data-item", label);
-  closeBtn.setAttribute("title", "close");
+  function createTag(label) {
+    const tagItem = document.createElement("div");
+    tagItem.setAttribute("class", "tag-item");
+    const span = document.createElement("span");
+    span.innerHTML = label;
+    const closeBtn = document.createElement("i");
+    closeBtn.setAttribute("class", "fas fa-times tag-item-close");
+    closeBtn.setAttribute("data-item", label);
+    closeBtn.setAttribute("title", "close");
 
-  div.appendChild(span);
-  div.appendChild(closeBtn);
-  return div;
-}
+    tagItem.appendChild(span);
+    tagItem.appendChild(closeBtn);
+    return tagItem;
+  }
 
-function reset() {
-  document.querySelectorAll(".tag").forEach(function (tag) {
-    tag.parentElement.removeChild(tag);
-  });
-}
-
-function addTags() {
-  reset();
-  tags
-    .slice()
-    .reverse()
-    .forEach(function (tag) {
-      const input = createTag(tag);
-      tagContainer.prepend(input);
+  function reset() {
+    tag.querySelectorAll(".tag-item").forEach(function (tag) {
+      tag.parentElement.removeChild(tag);
     });
-}
-
-input.addEventListener("keyup", function (e) {
-  if (e.key === "Enter") {
-    tags.push(input.value);
-    addTags();
-    input.value = "";
   }
-});
 
-document.addEventListener("click", function (e) {
-  if (e.target.tagName === "I") {
-    const value = e.target.getAttribute("data-item");
-    const index = tags.indexOf(value);
-    tags = [...tags.slice(0, index), ...tags.slice(index + 1)];
-    addTags();
+  function addTags() {
+    reset();
+    tagsData
+      .slice()
+      .reverse()
+      .forEach((tagData) => {
+        const input = createTag(tagData);
+        tagContainer.prepend(input);
+      });
   }
+
+  input.addEventListener("keyup", (e) => {
+    const duplicateFound = tagsData.filter((tagData) => {
+      return tagData == input.value;
+    }).length;
+    if (e.key === "Enter" && input.value && duplicateFound === 0) {
+      tagsData.push(input.value);
+      addTags();
+      input.value = "";
+    }
+  });
+
+  tag.addEventListener("click", (e) => {
+    if (e.target.tagName === "I") {
+      const value = e.target.getAttribute("data-item");
+      const index = tagsData.indexOf(value);
+      tagsData = [...tagsData.slice(0, index), ...tagsData.slice(index + 1)];
+      addTags();
+    }
+  });
 });
