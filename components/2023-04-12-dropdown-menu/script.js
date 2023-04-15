8d09190075692247406b06db28cc2a8feaaa3a4e
Andi Sultan <andisultan317@gmail.com>
2023-04-15 09:46:29 +0800

    2023-04-12-dropdown-menu | Close submenu if click outside

--- components/2023-04-12-dropdown-menu/script.js
+++ components/2023-04-12-dropdown-menu/script.js
@@ -9,6 +9,15 @@ const dropMenu = (() => {
         submenu.classList.toggle("show");
       });
     });
+
+    document.body.addEventListener("click", (e) => {
+      dropBtns.forEach((dropBtn) => {
+        if (e.target !== dropBtn && !e.target.closest(".menu-submenu")) {
+          const submenu = dropBtn.nextElementSibling;
+          submenu.classList.remove("show");
+        }
+      });
+    });
   };
 
   return { start };
