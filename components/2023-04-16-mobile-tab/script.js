4001347777dc0cc001a6a42a41ec75a1f3686592
Andi Sultan <andisultan317@gmail.com>
2023-04-20 23:44:28 +0800

    2023-04-16-mobile-tab | Add menu click event

--- components/2023-04-16-mobile-tab/script.js
+++ components/2023-04-16-mobile-tab/script.js
@@ -1,7 +1,29 @@
-const tab = (() => {
-  const start = (elemId) => {};
+// const pages = document.getElementById("page");
+const page = (() => {
+  const start = (elemId) => {
+    const page = document.getElementById(elemId);
+
+    const pageItems = page.querySelectorAll(".page-item");
+  };
 
   return { start };
 })();
 
+const tab = (() => {
+  const start = (elemId) => {
+    const tabMenu = document.getElementById(elemId);
+    const tabLinks = tabMenu.querySelectorAll(".tab-link");
+    tabLinks.forEach((tabLink) => {
+      tabLink.addEventListener("click", (e) => {
+        e.preventDefault();
+        const dataId = e.target.dataset.id;
+        // todo: click link to open page
+      });
+    });
+  };
+
+  return { start };
+})();
+
+page.start("page");
 tab.start("tab");
