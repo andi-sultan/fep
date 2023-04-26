0e18d7a078d537b08e3205f8ffaf81359a743af6
Andi Sultan <andisultan317@gmail.com>
2023-04-26 23:46:18 +0800

    2023-04-12-dropdown-menu | Add mouseover event on desktop

--- components/2023-04-12-dropdown-menu/script.js
+++ components/2023-04-12-dropdown-menu/script.js
@@ -5,16 +5,43 @@ const dropMenu = (() => {
     const dropBtns = menuElem.querySelectorAll(".menu-btn-drop");
     dropBtns.forEach((dropBtn) => {
       dropBtn.addEventListener("click", () => {
+        if (window.innerWidth < 720) {
+          const submenu = dropBtn.nextElementSibling;
+          submenu.classList.toggle("show");
+        }
+      });
+      dropBtn.addEventListener("mouseover", () => {
+        if (window.innerWidth >= 720) {
+          const submenu = dropBtn.nextElementSibling;
+          submenu.classList.add("show");
+        }
+      });
+      dropBtn.addEventListener("mouseleave", (e) => {
         const submenu = dropBtn.nextElementSibling;
-        submenu.classList.toggle("show");
+        if (window.innerWidth >= 720 && e.target.closest(".menu-submenu")) {
+          submenu.classList.remove("show");
+        }
       });
     });
 
     document.body.addEventListener("click", (e) => {
       dropBtns.forEach((dropBtn) => {
         if (e.target !== dropBtn && !e.target.closest(".menu-submenu")) {
-          const submenu = dropBtn.nextElementSibling;
-          submenu.classList.remove("show");
+          if (window.innerWidth < 720) {
+            const submenu = dropBtn.nextElementSibling;
+            submenu.classList.remove("show");
+          }
+        }
+      });
+    });
+
+    document.body.addEventListener("mouseover", (e) => {
+      dropBtns.forEach((dropBtn) => {
+        if (e.target !== dropBtn && !e.target.closest(".menu-submenu")) {
+          if (window.innerWidth >= 720) {
+            const submenu = dropBtn.nextElementSibling;
+            submenu.classList.remove("show");
+          }
         }
       });
     });
