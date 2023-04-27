c358db39be323bf8ae36797411cf8e044b6b4cad
Andi Sultan <andisultan317@gmail.com>
2023-04-27 23:19:58 +0800

    2023-04-27-img-slider

--- components/2023-04-27-img-slider/script.js
+++ components/2023-04-27-img-slider/script.js
@@ -8,4 +8,23 @@ const slider = (() => {
   return { start };
 })();
 
+const imgs = [
+  {
+    src: "https://images.unsplash.com/photo-1599394022918-6c2776530abb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1458&q=80",
+    alt: "1",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1593642632559-0c6d3fc62b89?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
+    alt: "2",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1599423300746-b62533397364?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
+    alt: "3",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1599561046251-bfb9465b4c44?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1492&q=80",
+    alt: "4",
+  },
+];
+
 slider.start("slider");
