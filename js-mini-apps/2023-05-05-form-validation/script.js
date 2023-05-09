6c776011e0aded22cadd36b6f0baf90b556b8587
Andi Sultan <andisultan317@gmail.com>
2023-05-09 14:56:31 +0800

    2023-05-05-form-validation | Update validation

--- js-mini-apps/2023-05-05-form-validation/script.js
+++ js-mini-apps/2023-05-05-form-validation/script.js
@@ -5,27 +5,48 @@ const form = (() => {
   const start = (elemId) => {
     elem = document.getElementById(elemId);
 
-    const name = elem.querySelector("#first-name");
-    const errMsg = name.nextElementSibling;
-
-    name.addEventListener("input", () => {
-      name.classList.add("input-text-validate");
-      if (name.validity.valueMissing) {
-        errMsg.classList.add("show");
-      } else {
-        errMsg.classList.remove("show");
-      }
+    const inputs = elem.querySelectorAll(".input-text");
+    inputs.forEach((input) => {
+      const errMsg = input.nextElementSibling;
+      // console.log(input.type);
+      input.addEventListener("input", () => {
+        input.classList.add("input-text-validate");
+        if (input.validity.valueMissing || input.validity.patternMismatch) {
+          errMsg.classList.add("show");
+          if (input.type == "email") {
+            errMsg.textContent = "* Please input valid email format";
+          } else if (input.type == "tel") {
+            errMsg.textContent = "* Please input valid phone number";
+          } else if (input.type == "password") {
+            errMsg.textContent =
+              "* Password is at least 8 characters with numbers and one of special character: [@$!%?&]";
+            const repeatPassword = elem.querySelector(
+              '[data-type="repeat-password"]'
+            );
+            if (input.value != repeatPassword.value) {
+            }
+          } else if (input.getAttribute("data-type") == "repeat-password") {
+          } else {
+            errMsg.textContent = "* Input Required";
+          }
+        } else {
+          errMsg.classList.remove("show");
+        }
+      });
     });
-    
+
     elem.addEventListener("submit", (e) => {
-      name.classList.add("input-text-validate");
       e.preventDefault();
 
-      if (name.validity.valueMissing) {
-        errMsg.classList.add("show");
-      } else {
-        errMsg.classList.remove("show");
-      }
+      inputs.forEach((input) => {
+        const errMsg = input.nextElementSibling;
+        input.classList.add("input-text-validate");
+        if (input.validity.valueMissing) {
+          errMsg.classList.add("show");
+        } else {
+          errMsg.classList.remove("show");
+        }
+      });
     });
   };
 
